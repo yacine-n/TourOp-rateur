309b5d141619d83dca301d825a05317da38ae570
Yacine Ndiaye <65970214+yacine-n@users.noreply.github.com>
2021-07-18 22:54:09 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*window.onload = function(){
+
+var conf = document.getElementById('conf');
+var redirect = document.getElementById('redirect');
+var tnom = document.getElementById('tnom');
+var submit = document.getElementById('submit');
+
+
+
+
+
+
+
+
+
+   
+
+}*/
